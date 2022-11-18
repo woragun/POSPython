5aeb011fe54ac3836ffdbb6756fffdaed0d8572b
woragun <woragun36308@gmail.com>
2022-11-18 09:58:44 +0700

    add title

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import tkinter as tk
-
+from tkinter import *
 class FMS:
     def __init__(self,root):
         self.root = root
-        self.root.geometry = ('1920x1080+0+0')
-        self.root.title = ('Farm Management System')
+        self.root.geometry('1920x1080+0+0')
+        self.root.title('Farm Management System')
+
+        title = Label(self.root,text='Farm Management System',font=('futura','40','bold')).place(x = 860,y = 50)
 
 
-root = tk.Tk()
+root = Tk()
 obj = FMS(root)
 root.mainloop()
