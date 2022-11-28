f8275fdfa6ea4469e9c42a8e45d65b2b4ebc14b9
woragun <woragun36308@gmail.com>
2022-11-28 13:39:09 +0700

    Add Clock

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ class FMS:
         self.root.geometry('1920x1080+0+0')
         self.root.title('Farm Management System')
 
-        title = Label(self.root,text='Farm Management System',font=('futura','40','bold')).place(x = 860,y = 50)
-
+        title = Label(self.root,text='Farm Management System',font=('futura','40','bold'),bg = '#517d30',fg='#fffde0').place(x = 720,y = 0)
+        
 
 root = Tk()
 obj = FMS(root)
