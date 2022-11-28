7e7aa8ac0d342d6054f33bc25c49ca710b22b234
woragun <woragun36308@gmail.com>
2022-11-28 14:51:54 +0700

    add images

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ class FMS:
         self.root = root
         self.root.geometry('1920x1080+0+0')
         self.root.title('Farm Management System')
+        self.root.configure(bg='#ffdeab')
 
-        title = Label(self.root,text='Farm Management System',font=('futura','40','bold'),bg = '#517d30',fg='#fffde0').place(x = 0,y = 0,relwidth=1)
-        self.lbl_clock = Label(self.root,text='Welcome to Farm Management System\t\t Date: DD-MM-YYYY\t\t Time:HH:MM',font=('futura','20','bold'),bg = '#517d30',fg='#fffde0')
-        self.lbl_clock.place(y=75,relwidth=1)
+        title = Label(self.root,text='Farm Management System',font=('futura','40','bold'),bg = '#517d30',fg='#fffde0').place(x = 0,y = 0,relwidth=1,height = 80)
+        self.lbl_clock = Label(self.root,text='Welcome to Sriwan Farm\t\t Date: DD-MM-YYYY\t\t Time:HH:MM',font=('futura','20','bold'),bg = '#517d30',fg='#fffde0')
+        self.lbl_clock.place(y=80,relwidth=1,height = 40)
 
+        Menu = Frame(self.root,relief = RIDGE,bg = '#517d30')
+        Menu.place(x=0, y=120,width = 384,height = 960)
+
+        self.home_img = Image.open('img\home.png')
+        self.home_img = self.home_img.resize((200,200))
+        self.home_img = ImageTk.PhotoImage((self.home_img))
         
+        btn_home = Button(Menu,image =self.home_img,bg = '#517d30',highlightthickness = 0, bd = 0).pack(side=TOP)
+        btn_storage = Button()
+
 
 root = Tk()
 obj = FMS(root)
